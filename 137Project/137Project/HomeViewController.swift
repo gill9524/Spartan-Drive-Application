6dbdb329474e1f0a25fc1ef195714a5d783126e8
Amrinder Giill <gillamrinder24@gmail.com>
2018-04-07 23:18:14 -0700

    Transition to Firebase
    Transition to Firebase from CloudKit, using cocoapods , when opening project open "137Project.xcworkspace" from now on.

--- 137Project/137Project/HomeViewController.swift
+++ 137Project/137Project/HomeViewController.swift
@@ -7,15 +7,11 @@
 //
 
 import UIKit
-import CloudKit
+
 
 class HomeViewController: UIViewController {
 
-    let database = CKContainer.default().privateCloudDatabase
     
-    var counter: Int = 0
-    
-    var notes = [CKRecord]()
     
     
     
@@ -25,41 +21,69 @@ class HomeViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    @IBAction func addToDatabase(_ sender: UIBarButtonItem) {
-        counter += 1
-        let record = CKRecord(recordType: "Note")
-        
-        record.setValue(String(counter), forKeyPath: "content")
-        
-        database.save(record) { (record, error) in
-            guard error == nil else { return print("Error")}
-            print("saved successfully")
-            
-        }
-    }
     
-    @IBAction func queryDatabase(_ sender: UIBarButtonItem) {
     
-        let query = CKQuery(recordType: "Note", predicate: NSPredicate(value: true))
-        database.perform(query, inZoneWith: nil) { (records, error) in
-            guard error == nil else { return }
-            self.notes = records!
-            for record in self.notes{
-                print(record.value(forKey: "content"))
-            }
-        }
-    }
     
-    @IBAction func removeFromDatabase(_ sender: UIBarButtonItem) {
-        let lastNote = self.notes.last
-        guard lastNote != nil else { return }
-        database.delete(withRecordID: (lastNote?.recordID)!) { (_, error) in
-            guard error == nil else { return }
-            let lastIndex = self.notes.count - 1
-            self.notes.remove(at: lastIndex)
-            print("deleted successfully.")
-        }
-    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //****************NOT************USING***********CLOUDKIT****************************//
+    
+    
+//    let database = CKContainer.default().privateCloudDatabase
+//
+//    var counter: Int = 0
+//
+//    var notes = [CKRecord]()
+//
+//    @IBAction func addToDatabase(_ sender: UIBarButtonItem) {
+//        counter += 1
+//        let record = CKRecord(recordType: "Note")
+//
+//        record.setValue(String(counter), forKeyPath: "content")
+//
+//        database.save(record) { (record, error) in
+//            guard error == nil else { return print("Error")}
+//            print("saved successfully")
+//
+//        }
+//    }
+//
+//    @IBAction func queryDatabase(_ sender: UIBarButtonItem) {
+//
+//        let query = CKQuery(recordType: "Note", predicate: NSPredicate(value: true))
+//        database.perform(query, inZoneWith: nil) { (records, error) in
+//            guard error == nil else { return }
+//            self.notes = records!
+//            for record in self.notes{
+//                print(record.value(forKey: "content"))
+//            }
+//        }
+//    }
+//
+//    @IBAction func removeFromDatabase(_ sender: UIBarButtonItem) {
+//        let lastNote = self.notes.last
+//        guard lastNote != nil else { return }
+//        database.delete(withRecordID: (lastNote?.recordID)!) { (_, error) in
+//            guard error == nil else { return }
+//            let lastIndex = self.notes.count - 1
+//            self.notes.remove(at: lastIndex)
+//            print("deleted successfully.")
+//        }
+//    }
     
     
 
--- 137Project/137Project/ViewController.swift
+++ 137Project/137Project/ViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import CloudKit
+
 
 class ViewController: UIViewController {
 
