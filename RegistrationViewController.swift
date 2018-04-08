fd2e983ebedfc8867a5af8b6e1b37d34464652f1
nicoleb1661 <anicolebaze1661@gmail.com>
2018-04-08 12:45:39 -0700

    Register Account
    A user can successfully register an account now. Firebase shows the user account in Authentication > Users. 
    
    Modifications to be done: After registration is complete the app goes to a black screen, need the next view to be the home screen.

---
+++ RegistrationViewController.swift
@@ -0,0 +1,110 @@
+//
+//  RegistrationViewController.swift
+//  137Project
+//
+//  Created by Amrinder Gill on 3/26/18.
+//  Copyright © 2018 Amrinder Gill. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class RegistrationViewController: UIViewController {
+    
+    //Outlets
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var newEmailTextField: UITextField!
+    @IBOutlet weak var newPasswordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    //Actions
+    @IBAction func cancelRegistrationTapped(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)   //Dismiss registration page
+    }
+    
+    @IBAction func registerButtonTapped(_ sender: UIButton) {
+        
+        //let username = firstNameTextField.text! + lastNameTextField.text!
+        let email = newEmailTextField.text
+        let password = newPasswordTextField.text
+        
+        //Validate required fields
+        if(firstNameTextField.text?.isEmpty)! || (lastNameTextField.text?.isEmpty)! || (newEmailTextField.text?.isEmpty)! || (newPasswordTextField.text?.isEmpty)! || (confirmPasswordTextField.text?.isEmpty)!
+        {
+            //Display Error fill out all textfields
+            displayAlert(userMessage: "All fields are required")
+            return
+        }
+        
+        //Validate password
+        if((newPasswordTextField.text?.elementsEqual(confirmPasswordTextField.text!))! != true)
+        {
+            //Display Password not Matching error
+            displayAlert(userMessage: "Password does not match")
+            return
+        }
+        
+        //Create Activity Indication
+        let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.gray)
+        //Position the acitivty indicatation in the center of the page
+        activityIndicator.center = view.center
+        
+        activityIndicator.hidesWhenStopped = false
+        
+        //Start activity indication
+        activityIndicator.startAnimating()
+        
+        view.addSubview(activityIndicator)
+        
+        //Creating user account in Firebase
+        Auth.auth().createUser(withEmail: email!, password: password!, completion: {(username, error) in
+            
+            //Error Handling
+            guard error == nil else{
+                self.displayAlert(userMessage: "Invalid")
+                return
+            }
+            
+            print(username?.email ?? "No Email")
+            print(username?.uid ?? "No ID")
+            
+            //Create and store user profile information
+            let changeRequest = username?.createProfileChangeRequest()
+            changeRequest?.commitChanges(completion: { (error) in
+                guard error == nil else{
+                    self.displayAlert(userMessage: "Error creating user profile")
+                    return
+                }
+                
+                self.performSegue(withIdentifier: "Registration View Controller", sender: nil)
+            })
+            
+        })
+        
+        
+        
+        
+    } //End registrationButtonTapped
+    
+    //Displays an alert message
+    func displayAlert(userMessage:String) -> Void {
+        DispatchQueue.main.async {
+            let alertControl = UIAlertController(title:"Alert", message: userMessage, preferredStyle: .alert)
+            
+            let OKAction = UIAlertAction(title: "OK", style: .default)
+            { (action:UIAlertAction!) in
+                DispatchQueue.main.async {
+                    //self.dismiss(animated: true, completion: nil)
+                    
+                }
+                
+            }
+            alertControl.addAction(OKAction)
+            self.present(alertControl, animated: true, completion: nil)
+        }
+    }
+    
+    
+}
